5748363a8b2f966be939924205f700ad902d1b42
Pyae Phyo Hlaing <www.pyaephyoehlaing16@gmail.com>
2023-11-23 11:04:51 +0630

    update

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://admin:Pyare132605@ucsbooks.t0qcvni.mongodb.net/UCSS');
+mongoose.connect('mongodb+srv://admin:Pyare132605@ucsbooks.t0qcvni.mongodb.net/UCSS',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.set('strictQuery', false);
 const port = process.env.PORT || 3000;
 const userRoute = require('./routes/userRoute');
 const bodyParser = require('body-parser');
