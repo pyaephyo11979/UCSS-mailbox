eb14b10b5651bd411ee47f327f518dbcf6e08ad3
Pyae Phyo Hlaing <www.pyaephyoehlaing16@gmail.com>
2023-11-28 07:53:36 +0630

    update

--- app.js
+++ app.js
@@ -18,8 +18,5 @@ app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Methods','*');
     next();
 })
-app.use(express.static('public'));
-app.set('view engine','ejs');
-app.set('views','./views')
 app.use('/',userRoute)
 app.listen(port, () => console.log(`Listening on port ${port}`));
